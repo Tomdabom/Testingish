1c4c670ba4b635980a018301c1617b54bad46cac
Thomas Frampton <tbframpt@ucalgary.ca>
2012-01-22 22:32:31 -0700

    test

---
+++ src/Cool.java
@@ -0,0 +1,17 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thomas Asus
+ * Date: 20/01/12
+ * Time: 11:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cool {
+                  int jk;
+    public Cool(){
+        jk = 8;
+        System.out.println("Cool!!");
+    }
+    
+
+
+}
---
+++ src/Testingish.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thomas Asus
+ * Date: 20/01/12
+ * Time: 10:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Testingish {
+    
+    public static void main(String[] args){
+        new Cool();
+    }
+}
